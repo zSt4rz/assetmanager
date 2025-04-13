dd33e5b9464c9cd036bff9049951e18c5d54e42a
Alan Deng <alan.j.deng@gmail.com>
2025-04-13 01:58:22 -0400

    update

--- app/user/inventory/page.js
+++ app/user/inventory/page.js
@@ -10,6 +10,7 @@ export default function InventoryPage() {
 
   const [images, setImages] = useState([])
 
+
   useEffect(() => {
     if (status === 'unauthenticated') {
       router.push('/login')
