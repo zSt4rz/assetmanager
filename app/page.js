93d20a5e9a42aca4d2b725290fe966948b94a969
Patrick Zhang <patrick8751@gmail.com>
2025-04-13 00:27:32 -0400

    floating
    lol

--- app/page.js
+++ app/page.js
@@ -5,9 +5,15 @@ export default async function Home() {
   return (
     <main className="min-h-screen bg-orange-100 flex justify-center items-center">
       <img src="/images/car.png" alt="car" className="h-36 conveyor-reverse top-[58%]"/>
-      <img src="/images/office-chair.png" alt="office chair" className="h-36 conveyor top-[18%]"/>
-      <h1 className="text-amber-950 italic text-9xl z-10">META<span className="font-bold">VAULT</span></h1>
+      <img src="/images/laptop.png" alt="laptop" className="h-36 conveyor-reverse top-[58%]" style={{ animationDelay: "1s" }}/>
 
+      <img src="/images/sofa.png" alt="sofa" className="h-36 conveyor top-[18%]" style={{ animationDelay: "1s" }}/>
+
+      <img src="/images/office-chair.png" alt="office chair" className="h-36 conveyor top-[18%]"/>
+      <img src="/images/chair.png" alt="chair" className="h-36 conveyor top-[18%]" style={{ animationDelay: "1s" }}/>
+
+
+      <h1 className="text-amber-950 italic text-9xl z-10">META<span className="font-bold">VAULT</span></h1>
     </main>
   )
 
